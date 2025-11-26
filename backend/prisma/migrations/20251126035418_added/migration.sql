945b20dcf52a4ba0adc821ebddfa0eb869663e6c
ambuj vashistha <ambujva123@gmail.com>
2025-11-26 09:24:34 +0530

    added index.js

---
+++ backend/prisma/migrations/20251126035418_added/migration.sql
@@ -0,0 +1,72 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Authors` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Books` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Genres` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Maps` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE `Authors`;
+
+-- DropTable
+DROP TABLE `Books`;
+
+-- DropTable
+DROP TABLE `Genres`;
+
+-- DropTable
+DROP TABLE `Maps`;
+
+-- CreateTable
+CREATE TABLE `Book` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `summary` VARCHAR(191) NOT NULL,
+    `isbn` VARCHAR(191) NOT NULL,
+    `url` VARCHAR(191) NOT NULL,
+    `authorId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Author` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(191) NOT NULL,
+    `family_name` VARCHAR(191) NOT NULL,
+    `date_of_birth` DATETIME(3) NOT NULL,
+    `date_of_death` DATETIME(3) NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `lifeSpan` VARCHAR(191) NULL,
+    `url` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Genre` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `url` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `BookGenreMapping` (
+    `bookId` INTEGER NOT NULL,
+    `genreId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`bookId`, `genreId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Book` ADD CONSTRAINT `Book_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `Author`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `BookGenreMapping` ADD CONSTRAINT `BookGenreMapping_bookId_fkey` FOREIGN KEY (`bookId`) REFERENCES `Book`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `BookGenreMapping` ADD CONSTRAINT `BookGenreMapping_genreId_fkey` FOREIGN KEY (`genreId`) REFERENCES `Genre`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
